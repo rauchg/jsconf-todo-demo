fd9cf951283093159bbd4162728f584ed878add1
Guillermo Rauch <rauchg@gmail.com>
2010-09-26 09:52:46 +0200

    Moved requires into server.js for no.de deployment

--- run.js
+++
@@ -1,9 +0,0 @@
-// some paths, should not be necessary with npm
-require.paths.unshift(__dirname + '/support/express/lib/',
-                      __dirname + '/support/express/support/connect/lib',
-                      __dirname + '/support/socket.io/lib/',
-                      __dirname + '/support/nodestream/lib/',
-                      __dirname + '/support/mongoose/',
-                      __dirname + '/support/jade/lib/');
-
-require('./server');
--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-// requires
+// require.paths are needed until these projects become stable in npm
+require.paths.unshift(__dirname + '/support/express/lib/',
+                      __dirname + '/support/express/support/connect/lib',
+                      __dirname + '/support/socket.io/lib/',
+                      __dirname + '/support/nodestream/lib/',
+                      __dirname + '/support/mongoose/',
+                      __dirname + '/support/jade/lib/');
+
 var express = require('express'),
     mongoose = require('mongoose').Mongoose,
     db = mongoose.connect('mongodb://localhost/todo'),
